b801cb9e1d28338b7216b1710fd202517823dcdc
Moshiur Rahman <mrahman9@upei.ca>
2021-05-27 23:58:10 -0300

    database connection established

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 
+//mongoose database
+const mongoose = require("mongoose");
+
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+//setting up database connection
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+	console.log("MongoDB database connection established successfully");
+});
 
 app.use(cors);
 app.use(express.json());
